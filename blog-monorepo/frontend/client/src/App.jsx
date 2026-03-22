728d55a975f9bcb4c9deb67fb8b82eadd5654376
stevenstank <stevenstank775@gmail.com>
2026-03-23 01:01:48 +0530

    add footer component with github link

--- blog-monorepo/frontend/client/src/App.jsx
+++ blog-monorepo/frontend/client/src/App.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Post from './pages/Post';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:id" element={<Post />} />
-    </Routes>
+    <div className="app-shell">
+      <div className="app-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<Post />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
